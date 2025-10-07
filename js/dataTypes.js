cb69f43650bf4571d3ab72f85e713a9159bf58dd
prasanth <prasanth8142@gmail.com>
2025-10-07 17:39:17 +0530

    primitive data types completed

--- js/dataTypes.js
+++ js/dataTypes.js
@@ -9,3 +9,33 @@ const c =true
 console.log(a)
 console.log(b)
 console.log(c)
+// String 
+// Number
+// Boolean
+
+var d = "This is a string @$123"
+var e='This one also a string'
+let f = 15.65
+console.log(typeof d)
+console.log(e)
+console.log(f)
+// undefined
+
+var Apple="Mango"
+var Chai=Apple
+console.log(Chai)
+
+var h
+console.log(h)
+
+
+
+
+// null
+var g=null
+console.log(g)
+
+
+// var global scope
+//  let block scope
+//  const block scope
