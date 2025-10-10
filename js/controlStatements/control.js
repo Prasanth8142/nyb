96497bed89f656287a2f71ab0b9b9d3b66b09374
prasanth <prasanth8142@gmail.com>
2025-10-10 17:08:10 +0530

    Controlstatements

---
+++ js/controlStatements/control.js
@@ -0,0 +1,44 @@
+var a=10
+if(a=10){
+console.log("true")
+}
+
+//else if
+let b=20
+if(b%2!=0){
+    console.log("20 is a Even Number")
+}
+else{
+    console.log("20 is a odd number")
+}
+
+
+//switch
+let c=10;let d=20
+if(c!=10){
+    console.log("c ==10")
+}
+else if(c==5 && d==20){
+    console.log("c & d are trues")
+}
+else{
+    console.log("false")
+}
+
+
+//switch
+var fruit="chai"
+switch(fruit){
+    case  'bananna':
+    console.log("this is a banana")
+    break
+    case 'mango':
+        console.log("This is a mango")
+        break
+        case 'apple':
+            console.log("this is a apple")
+            break
+            default:
+                console.log("not a fruit")
+
+}
